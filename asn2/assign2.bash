226a27847ea97a19c2746b0694e3925cd8f3ae82
Andrew DiCarlo <andrew.dicarlo@gmail.com>
2020-03-31 15:28:33 -0500

    Changed to run off one .sed file rather than multiple

--- asn2/assign2.bash
+++ asn2/assign2.bash
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-#define list of sed files
-command_list=(tokens.sed binary_ops.sed lines.sed conditions.sed braces.sed)
-#tokens.sed: Makes sure tokens are separated by exactly one space
-#binary_ops.sed: Makes sure operators are surrounded by exactly one space on either side
-#lines.sed: Strips trailing whitespace at the end of lines
-#conditions.sed: Makes sure there are no spaces before conditions, or immediately inside parenthesis
-#braces.sed: Puts one space before any opening brace that is not on it's own line
+sedfile=commands.sed
 
 #Check for proper usage of command
 if [[ $# -lt 1 ]]; then
@@ -27,9 +21,7 @@ outfile=$(echo "$1" | sed -E -f outfile_fmt.sed)
 
 cat "$infile" >"$outfile"
 
-for sedfile in "${command_list[@]}"; do
-    sed -i.bak -E -f "$sedfile" "$outfile"
-done
+sed -i.bak -E -f "$sedfile" "$outfile"
 
 cat "$outfile"
 
